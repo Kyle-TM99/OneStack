0ee7a5f3226134f0aac4424174c6012c9d47033b
DGM <dogun0224@naver.com>
2025-01-08 17:31:56 +0900

    1. mysql.sql - Member 속성 수정 2. dummy.sql - 설문조사 더미데이터 추가 3. ProfessionalController - 컨트롤러 수정 4. surveyPage.html - view 페이지 제공답변 출력 완료

--- src/main/java/com/onestack/project/controller/ProfessionalController.java
+++ src/main/java/com/onestack/project/controller/ProfessionalController.java
@@ -1,7 +1,10 @@
 package com.onestack.project.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import com.onestack.project.domain.SurveyWithCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,20 +17,21 @@ import com.onestack.project.service.SurveyService;
 
 @Controller
 public class ProfessionalController {
-	
+
 	@Autowired
 	private SurveyService surveyService;
-	
+
 	@GetMapping("/survey")
 	public String getSurveyForm(Model model, @RequestParam(value = "itemNo", required = false, defaultValue = "11") int itemNo) {
-		
+
 		// 설문조사 질문 리스트, 제공 답변 가져오기 - 제공 답변은 split이용
 		Map<String, Object> modelMap = surveyService.getSurvey(itemNo);
-				
+
 		model.addAllAttributes(modelMap);
-		
+
 		return "views/surveyPage";
+
 	}
-	
+
 
 }
