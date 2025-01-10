36002e5996298e0529a41411922f8890b763d4bc
hunbok <rmsidrmfo1537@gmail.com>
2025-01-10 15:52:38 +0900

    [LHB] reviewProcessingDetails.html - 심사 처리 조회, 승인 수정 기능 [완료] members.js - 심사 수정 기능 추가 및 모달 초기화 구현, 공통 닫기버튼 이벤트 추가 [완료] (부트스트랩 모달닫기 버튼 기본이벤트 사용 X) adminController - /updateReviewPro 호출 메서드 추가 [완료]

--- src/main/java/com/onestack/project/mapper/ManagerMapper.java
+++ src/main/java/com/onestack/project/mapper/ManagerMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.onestack.project.domain.MemProPortCate;
 import com.onestack.project.domain.Member;
 import com.onestack.project.domain.Professional;
 
@@ -13,10 +14,10 @@ public interface ManagerMapper {
 	
 	public List<Member> getAllMember();
 	
-	public List<Professional> getRequestPro();
+	public List<MemProPortCate> getMemProPortCate();
 	
 	void updateProStatus(@Param("proNo") int proNo, 
-            @Param("status") Integer status, 
+            @Param("professorStatus") Integer professorStatus, 
             @Param("screeningMsg") String screeningMsg);
 
 }
