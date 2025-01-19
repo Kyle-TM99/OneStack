89e38c2a3031e68f1effa5f7a89937f57afcfacb
dgm <dogun0224@naver.com>
2025-01-19 16:28:58 +0900

    충돌 해결

--- src/main/java/com/onestack/project/controller/ProfessionalController.java
+++ src/main/java/com/onestack/project/controller/ProfessionalController.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-
 import com.onestack.project.domain.Estimation;
 import com.onestack.project.domain.MemProAdInfoCate;
 import com.onestack.project.domain.Professional;
@@ -15,10 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.onestack.project.domain.ProConversionRequest;
 import com.onestack.project.service.MemberService;
